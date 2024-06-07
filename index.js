d34cbdfd7d8f40269b41e6797d4615a0c6ebafe5
Judelbugayong <Judelbugayong2@gmail.com>
2024-06-07 21:28:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function hitLikes1() {
 }
 btnLike1.addEventListener("click",hitLikes1)
 
+
 function hitLikes2() {
    let totalLikes2 = parseInt(countLikes2.value) + 1
    countLikes2.textContent = totalLikes2.toString()
